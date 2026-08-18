820d3f59b178a0f5655d27aec5ea702490cad722
Max Dai <maxdai510@gmail.com>
2026-08-18 15:49:31 -0700

    Initial LCD code - SPI communication with LCD - custom graphics renderer - can print strings of different sizes, rectangles currently - added gitignore - removed build artifacts from git

--- Debug/Core/ThirdParty/kiss-fft/kiss_fft.d
+++
@@ -1,8 +0,0 @@
-Core/ThirdParty/kiss-fft/kiss_fft.o: \
- ../Core/ThirdParty/kiss-fft/kiss_fft.c \
- ../Core/ThirdParty/kiss-fft/_kiss_fft_guts.h \
- ../Core/ThirdParty/kiss-fft/kiss_fft.h \
- ../Core/ThirdParty/kiss-fft/kiss_fft_log.h
-../Core/ThirdParty/kiss-fft/_kiss_fft_guts.h:
-../Core/ThirdParty/kiss-fft/kiss_fft.h:
-../Core/ThirdParty/kiss-fft/kiss_fft_log.h:
